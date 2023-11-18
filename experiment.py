8ef901cc4ed1a854e0bdb93c5166b9077aafb24e
Will Wolf <williamabrwolf@gmail.com>
2023-11-18 13:50:07 -0500

    pretty print results

--- experiment.py
+++ experiment.py
@@ -1,10 +1,14 @@
+import itertools
+from pprint import pprint
 from mcts import stub, MCTS
 from util import parse_args
 
 
-K = [1, 2, 3]
-NUM_ROLLOUTS = [1, 2, 3]
-PARAMS = [(k, nr) for k in K for nr in NUM_ROLLOUTS]
+PARAM_RANGES = {
+    "K": [1, 2],
+    "num_rollouts": [1, 2],
+}
+PARAMS = list(itertools.product(*PARAM_RANGES.values()))
 
 
 if __name__ == "__main__":
@@ -16,5 +20,9 @@ if __name__ == "__main__":
             args.debug,
             args.dry,
         )
-        results = list(mcts.run.starmap(PARAMS))
-        print(results)
+        output = mcts.run.starmap(PARAMS)
+        results = [
+            {**res, "params": dict(zip(PARAM_RANGES.keys(), vals))}
+            for res, vals in zip(output, PARAMS)
+        ]
+        pprint(results)
